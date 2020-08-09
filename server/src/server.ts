0774dca3d55012c5a7400656d7fffe91b088c20f
João Vitor Müller <muller.joaovitor00@gmail.com>
2020-08-09 17:53:04 -0300

    fix: bugs correction

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,6 +1,10 @@
 import express from 'express';
+import * as dotenv from 'dotenv';
 import cors from 'cors';
 import routes from './routes';
+
+dotenv.config();
+const PROFFY_SERVER_PORT = 3333;
 
 const app = express();
 
@@ -8,4 +12,6 @@ app.use(cors());
 app.use(express.json());
 app.use(routes);
 
-app.listen(3333);
+app.listen(PROFFY_SERVER_PORT, () => {
+    console.log(`[*] Server running on ${PROFFY_SERVER_PORT}`);
+});
